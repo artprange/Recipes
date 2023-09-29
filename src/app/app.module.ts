ac6d72904bb84e698039df7d1985a02c9f78dd7b
xprangex <arthur.prange@gmail.com>
2023-09-29 19:30:34 -0300

    feature(#003): creating recipes-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
+import { RecipesListComponent } from './recipes-list/recipes-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipesComponent
+    RecipesComponent,
+    RecipesListComponent
   ],
   imports: [
     BrowserModule,
